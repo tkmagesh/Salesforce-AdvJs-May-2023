7275607c7929c05add057c672f8fe1281e056fc6
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-02 00:31:34 +0530

    updates

--- 01-language-foundation/add.spec.js
+++ 01-language-foundation/add.spec.js
@@ -1,6 +1,13 @@
 
 function add(x,y){
     function parseArg(n){
+        if (Array.isArray(n)){
+            var result = 0;
+            for (var idx = 0; idx < n.length; idx++){
+                result += parseArg(n[idx])
+            }
+            return result;
+        }
         return isNaN(n) ? 0 : parseInt(n);
     }
     var result = 0
@@ -39,3 +46,15 @@ test("add(10,20,30,40,50) => 150", function () {
     var actualResult = add(10,20,30,40,50)
     expect(actualResult).toBe(expectedResult)
 }) 
+
+test("add([10,20,30],[40,50,60]) //=> 210", function(){
+    var expectedResult = 210
+    var actualResult = add([10, 20, 30], [40, 50, 60])
+    expect(actualResult).toBe(expectedResult)
+})
+
+test("add([10,'20',30],[40,50,'abc']) //=> 150", function () {
+    var expectedResult = 150
+    var actualResult = add([10, '20', 30], [40, 50, 'abc'])
+    expect(actualResult).toBe(expectedResult)
+})

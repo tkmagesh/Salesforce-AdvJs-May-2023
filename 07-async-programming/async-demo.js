ef9a3e534cd1ab6627c27816c6e60b5868165b4c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-03 05:19:16 +0530

    handling errors in async operations (callback)

--- 07-async-programming/async-demo.js
+++ 07-async-programming/async-demo.js
@@ -18,21 +18,75 @@
 
     window['addSyncClient'] = addSyncClient;
 
-    //async
-    function addAsync(x, y) {
+    //handling errors
+    function divideSync(x, y) {
+        console.log(`   [@service] processing ${x} and ${y}`)
+        if (y === 0){
+            throw new Error('invalid arguments. divisor cannot be 0')
+        }
+        const result = x / y
+        console.log(`   [@service] returning result`)
+        return result
+    }
+
+    function divideSyncClient() {
+        try {
+            console.log(`[@client] invoking the service`)
+            const result = divideSync(100, 0)
+            console.log(`[@client] result = ${result}`)
+        } catch (e){
+            console.log(`[@client] error occurred : ${e}`)
+        }
+    }
+
+    window['divideSyncClient'] = divideSyncClient;
+
+    //async (using callback)
+    function addAsync(x, y, callback) {
         console.log(`   [@service] processing ${x} and ${y}`)
         setTimeout(() => {
             const result = x + y
             console.log(`   [@service] returning result`)
-            return result    
-        }, 5000);
+            // return result    
+            callback(result)
+        });
     }
 
     function addAsyncClient() {
         console.log(`[@client] invoking the service`)
-        const result = addAsync(100, 200)
-        console.log(`[@client] result = ${result}`)
+        addAsync(100, 200, function(result){
+            console.log(`[@client] result = ${result}`)  
+        })
     }
 
     window['addAsyncClient'] = addAsyncClient;
+
+    //handling errors
+    function divideAsync(x, y, callback) {
+        console.log(`   [@service] processing ${x} and ${y}`)
+        setTimeout(() => {
+            if (y === 0) {
+                // throw new Error('invalid arguments. divisor cannot be 0')
+                const err = new Error('invalid arguments. divisor cannot be 0')
+                callback(err, undefined)
+                return
+            }
+            const result = x / y
+            console.log(`   [@service] returning result`)
+            callback(undefined, result)
+        }, 5000);
+    }
+
+    function divideAsyncClient() {
+        console.log(`[@client] invoking the service`)
+        divideAsync(100, 0, function(err, result){
+            if (err){
+                console.log(`[@client] error occurred : ${err}`)
+                return
+            }
+            console.log(`[@client] result = ${result}`)
+        })        
+    }
+    window['divideAsyncClient'] = divideAsyncClient;
+
 })()

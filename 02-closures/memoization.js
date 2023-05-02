140d17848ecec38da399d3e78e9f1c7b4b50dd32
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-02 21:54:29 +0530

    memoization - update

--- 02-closures/memoization.js
+++ 02-closures/memoization.js
@@ -31,6 +31,7 @@ var isEven = (function () {
     }
 })()
 
+/* 
 function memoize(fn) {
     var results = {}
     return function (no) {
@@ -39,7 +40,19 @@ function memoize(fn) {
         results[no] = fn(no)
         return results[no]
     }
-}
+} 
+*/
+
+function memoize(fn, ctx) {
+    var results = {}
+    return function () {
+        var key = JSON.stringify(arguments)
+        if (results.hasOwnProperty(key)) return results[key]
+        console.log('processing :', key)
+        results[key] = fn.apply(ctx, arguments)
+        return results[key]
+    }
+} 
 
 var isEven = memoize(function (no) { return no % 2 === 0; })
 
@@ -55,3 +68,18 @@ var isPrime = memoize(function isPrime(no) {
 // Modify the memoize function in such a way that it can memoize the result of any function with any number of arguments
 // for example, the following should work
 var memoizedAdd = memoize(function (x, y) { return x + y; })
+memoizedAdd(10,20)
+memoizedAdd(10, 200)
+memoizedAdd(100, 20)
+
+
+var memoizedAdd = memoize(function () {
+    var result = 0;
+    for (var idx = 0; idx < arguments.length; idx++)
+        result += arguments[idx]
+    return result
+})
+
+memoizedAdd(10)
+memoizedAdd(10, 20)
+memoizedAdd(10, 20, 30)

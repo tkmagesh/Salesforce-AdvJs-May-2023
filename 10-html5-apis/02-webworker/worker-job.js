92bce9be9ed6bfeacd36fcad4f5ff5ede36a806c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-05-04 04:52:10 +0530

    web workers

---
+++ 10-html5-apis/02-webworker/worker-job.js
@@ -0,0 +1,26 @@
+console.log('worker-job loaded')
+
+function doWork() {
+    for (let i = 1; i <= 10000; i++){
+        for (let j = 0; j < 1000; j++)
+            for (let k = 0; k < 2000; k++) {
+
+            }
+        if (i % 100 === 0){
+            const percentCompleted = (i / 10000) * 100
+            console.log(percentCompleted)
+            self.postMessage({
+                type : 'progress',
+                percentCompleted
+            })
+        }
+    }
+}
+
+self.addEventListener('message', evtArg => {
+    if (evtArg.data.type === 'start'){
+        console.log('starting doWork')
+        doWork()
+        self.postMessage({type : 'completed'})
+    }
+})
